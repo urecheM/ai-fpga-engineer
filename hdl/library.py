bb2354dc4f50e4e22337041ebad74c7bd9b0a369
urecheM <urecheclara24@gmail.com>
2026-06-29 21:29:15 -0700

    Add imports and future annotations to library.py

---
+++ hdl/library.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from typing import Callable
+import math
+import random
+
+from ..core.spec import Specification, Port
+from ..core.decisions import ArchDecisions, 
